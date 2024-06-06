33ce8342cd27b3e3658d85f94b333c43ca9520ef
Marco Conciatori <conciatori.marco91@gmail.com>
2024-06-06 14:46:03 +0200

    file renamer changed I/O

--- main.py
+++ main.py
@@ -31,15 +31,21 @@ def get_args(default_args_dict: dict) -> dict:
 
     input_folder_path = args_dict['input_folder_path']
     if input_folder_path == '':
-        input_folder_path = input('Please insert path to the video folder:')
+        input_folder_path = input('Insert path to the video folder:')
     output_folder_path = args_dict['output_folder_path']
+    if output_folder_path == '':
+        output_folder_path = input('Insert path to the output folder (press enter to use the same as input):')
     if output_folder_path == '':
         output_folder_path = input_folder_path
     name_prefix = args_dict['name_prefix']
+    if name_prefix == '':
+        name_prefix = input('Insert prefix for file names (press enter to skip):')
     if name_prefix != '':
         if name_prefix[-1] != '_':
             name_prefix += '_'
     name_suffix = args_dict['name_suffix']
+    if name_suffix == '':
+        name_suffix = input('Insert suffix for file names (press enter to skip):')
     if name_suffix != '':
         if name_suffix[0] != '_':
             name_suffix = f'_{name_suffix}'
@@ -49,6 +55,9 @@ def get_args(default_args_dict: dict) -> dict:
         new_convert_only_format.append(file_format.lower())
     convert_only_format = new_convert_only_format
     keep_original = args_dict['keep_original']
+    keep_original_str = ('Keep original files? (y/n):')
+    if keep_original_str.lower() == 'y' or keep_original_str.lower() == 'yes':
+        keep_original = True
 
     temp_args_dict['input_folder_path'] = input_folder_path
     temp_args_dict['output_folder_path'] = output_folder_path
@@ -66,6 +75,18 @@ def rename_files(**kwargs):
     name_suffix = kwargs['name_suffix']
     convert_only_format = kwargs['convert_only_format']
     keep_original = kwargs['keep_original']
+    if kwargs['verbose']:
+        print('File renamer is running...')
+        print(f' - Select files from: {input_folder_path}')
+        print(f' - Put renamed files in: {output_folder_path}')
+        if name_prefix != '':
+            print(f' - Prefix added to all file names: {name_prefix}')
+        if name_suffix != '':
+            print(f' - Suffix added to all file names: {name_suffix}')
+        if len(convert_only_format) > 0:
+            print(f' - Convert only the following formats: {convert_only_format}')
+        print(f' - Keep original file/s: {keep_original}')
+    counter = 0
     with os.scandir(input_folder_path) as dir_entry_iterator:
         for entry in dir_entry_iterator:
             if entry.is_file():
@@ -84,6 +105,10 @@ def rename_files(**kwargs):
                     shutil.copy(src=entry.path, dst=f'{output_folder_path}{new_file_name}')
                 else:
                     os.rename(src=entry.path, dst=f'{output_folder_path}{new_file_name}')
+                counter += 1
+
+    if kwargs['verbose']:
+        print(f'Execution completed: {counter} files have been renamed.')
 
 
 if __name__ == '__main__':
@@ -94,7 +119,9 @@ if __name__ == '__main__':
         'name_suffix': '',
         'convert_only_format': [],
         'keep_original': False,
+        'verbose': True,
     }
     # D:/GIT/file_renamer/data/
     updated_kwargs = get_args(default_args_dict=kwargs)
     rename_files(**updated_kwargs)
+    input('Press any key to exit...')
