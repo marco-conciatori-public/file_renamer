e32daac887d3e0ec5482b55bee2ff26197d4f2ca
Marco Conciatori <conciatori.marco91@gmail.com>
2024-06-07 16:31:48 +0200

    changed logic, persistent window

--- file_renamer_gui.py
+++ file_renamer_gui.py
@@ -5,142 +5,242 @@ from datetime import datetime
 from tkinter import filedialog
 
 
-def pretty_print_dict(data, _level: int = 0):
-    if type(data) == dict:
-        for key in data:
-            for i in range(_level):
-                print('\t', end='')
-            print(f'{key}:')
-            pretty_print_dict(data[key], _level=_level + 1)
+def add_text_to_string_var(string_var, text):
+    temp_text = string_var.get()
+    if temp_text == '':
+        string_var.set(text)
     else:
-        for i in range(_level):
-            print('\t', end='')
-        print(data)
+        string_var.set(f'{temp_text}\n{text}')
 
 
-def get_args_gui() -> dict:
+def file_renamer_gui_():
     # create root window
     root = Tk()
     # root window title and dimension
-    root.title("File Renamer")
-    root.geometry('560x250')
-    # root.configure()
-
-    input_frame = Frame(root)
-    input_frame.pack(side=TOP, fill=BOTH, expand=True)
+    root.title('File Renamer')
+    # root.geometry('560x450')
 
     columnspan = 4
     pady = 10
+    padx = 10
+
+    # FRAME 1
+    input_frame = Frame(root)
+    input_frame.pack(side=TOP, fill=X, expand=False)
+
     # input_folder_path
     row = 0
-    Label(input_frame, text="Path to the video folder:").grid(column=0, row=row)
-    input_folder_path = StringVar(input_frame, value='Select folder')
+    Label(input_frame, text='Path to the video folder:').grid(column=0, row=row, sticky=W)
+    input_folder_path = StringVar(input_frame, value='')
 
     def browse_input_folders():
-        folder_path = filedialog.askdirectory(initialdir="/", title="Select a Folder")
+        folder_path = filedialog.askdirectory(initialdir='/', title='Select a Folder', mustexist=True)
         input_folder_path.set(folder_path)
-    Entry(input_frame, width=40, textvariable=input_folder_path).grid(column=1, row=row, columnspan=columnspan, padx=10)
-    Button(input_frame, text="Browse", command=browse_input_folders).grid(column=2 + columnspan, row=row)
+    Entry(input_frame, width=40, textvariable=input_folder_path).grid(
+        column=1,
+        row=row,
+        columnspan=columnspan,
+        padx=10,
+        pady=pady,
+    )
+    Button(input_frame, text='Browse', command=browse_input_folders).grid(
+        column=2 + columnspan,
+        row=row,
+        pady=pady,
+        padx=padx,
+    )
 
     # output_folder_path
     row += 1
-    Label(input_frame, text="Path to destination folder: \n(Leave empty to use the same as input)").grid(column=0, row=row)
-    output_folder_path = StringVar(input_frame, value='Leave empty to use the same as input')
+    Label(input_frame, text='Path to destination folder: (Leave empty to use the same as input)', wraplength=200).grid(
+        column=0,
+        row=row,
+        sticky=W,
+    )
+    output_folder_path = StringVar(input_frame, value='')
 
     def browse_output_folders():
-        folder_path = filedialog.askdirectory(initialdir="/", title="Select a Folder")
+        folder_path = filedialog.askdirectory(initialdir='/', title='Select a Folder', mustexist=True)
         output_folder_path.set(folder_path)
-    Entry(input_frame, width=40, textvariable=output_folder_path).grid(column=1, row=row, columnspan=columnspan, padx=10)
-    Button(input_frame, text="Browse", command=browse_output_folders).grid(column=2 + columnspan, row=row)
+    Entry(input_frame, width=40, textvariable=output_folder_path).grid(
+        column=1,
+        row=row,
+        columnspan=columnspan,
+        padx=10,
+        pady=pady,
+    )
+    Button(input_frame, text='Browse', command=browse_output_folders).grid(
+        column=2 + columnspan,
+        row=row,
+        pady=pady,
+        padx=padx,
+    )
 
     # name_prefix
     row += 2
-    Label(input_frame, text="Prefix for file names (optional):").grid(column=0, row=row, pady=pady)
+    Label(input_frame, text='Prefix for file names (optional):').grid(column=0, row=row, pady=pady, sticky=W)
     name_prefix = StringVar(input_frame, value='')
-    Entry(input_frame, width=40, textvariable=name_prefix).grid(column=1, row=row, columnspan=columnspan)
+    Entry(input_frame, width=40, textvariable=name_prefix).grid(column=1, row=row, columnspan=columnspan, pady=pady)
 
     # name_suffix
     row += 1
-    Label(input_frame, text="Suffix for file names (optional):").grid(column=0, row=row, pady=pady)
+    Label(input_frame, text='Suffix for file names (optional):').grid(column=0, row=row, pady=pady, sticky=W)
     name_suffix = StringVar(input_frame, value='')
-    Entry(input_frame, width=40, textvariable=name_suffix).grid(column=1, row=row, columnspan=columnspan)
+    Entry(input_frame, width=40, textvariable=name_suffix).grid(column=1, row=row, columnspan=columnspan, pady=pady)
 
     # convert_only_format
     # row += 1
-    # Label(input_frame, text="Convert only the following formats (optional):").grid(column=0, row=row)
+    # Label(input_frame, text='Convert only the following formats (optional):').grid(
+    #     column=0,
+    #     row=row,
+    #     pady=pady,
+    #     sticky=W,
+    # )
     # convert_only_format = StringVar(input_frame, value='')
-    # Entry(input_frame, width=40, textvariable=convert_only_format).grid(column=1, row=row)
+    # Entry(input_frame, width=40, textvariable=convert_only_format).grid(column=1, row=row, pady=pady)
 
     # keep_original
     row += 1
-    Label(input_frame, text="Keep original files?").grid(column=0, row=row, pady=pady)
+    Label(input_frame, text='Keep original files?').grid(column=0, row=row, pady=pady, sticky=W)
     keep_original_int = IntVar(input_frame, value=0)
-    Radiobutton(input_frame, text="Yes", value=1, variable=keep_original_int).grid(column=1, row=row)
-    Radiobutton(input_frame, text="No", value=0, variable=keep_original_int).grid(column=2, row=row)
+    Radiobutton(input_frame, text='Yes', value=1, variable=keep_original_int).grid(column=1, row=row, pady=pady)
+    Radiobutton(input_frame, text='No', value=0, variable=keep_original_int).grid(column=2, row=row, pady=pady)
 
     # verbose
     # row += 1
-    # Label(input_frame, text="Verbose?").grid(column=0, row=row)
+    # Label(input_frame, text='Verbose?').grid(column=0, row=row, sticky=W)
     # verbose_int = IntVar(input_frame, value=0)
-    # Radiobutton(input_frame, text="Yes", value=1, variable=verbose_int).grid(column=1, row=row)
-    # Radiobutton(input_frame, text="No", value=0, variable=verbose_int).grid(column=2, row=row)
+    # Radiobutton(input_frame, text='Yes', value=1, variable=verbose_int).grid(column=1, row=row)
+    # Radiobutton(input_frame, text='No', value=0, variable=verbose_int).grid(column=2, row=row)
     # verbose = bool(verbose_int.get())
 
+    # FRAME 2
     control_frame = Frame(root)
     control_frame.pack(side=BOTTOM, fill=X, expand=False)
 
-    # ok and cancel buttons
-    Button(control_frame, text="OK", width=10, command=root.destroy).pack(side=LEFT, padx=135, pady=pady)
-    Button(control_frame, text="Cancel", width=10, command=root.destroy).pack(side=LEFT, padx=0)
+    def rename_files_local_args():
+        try:
+            rename_files(
+                input_folder_path=input_folder_path.get(),
+                output_folder_path=output_folder_path.get(),
+                name_prefix=name_prefix.get(),
+                name_suffix=name_suffix.get(),
+                convert_only_format=[],
+                keep_original=bool(keep_original_int.get()),
+                verbose=True,
+                console_output_channel=console_output_channel,
+            )
+        except Exception as e:
+            add_text_to_string_var(string_var=console_output_channel, text=e)
+        ok_button_handle.config(state=DISABLED)
+    # ok and exit buttons
+    ok_button_handle = Button(control_frame, text='OK', width=10, command=rename_files_local_args)
+    ok_button_handle.pack(side=LEFT, padx=135, pady=pady)
+    Button(control_frame, text='Exit', width=10, command=root.destroy).pack(side=LEFT, padx=0, pady=pady)
+
+    # FRAME 3
+    console_output_frame = Frame(root)
+    console_output_frame.pack(side=BOTTOM, fill=BOTH, expand=True)
+    # console output
+    console_output_channel = StringVar(console_output_frame, value='Console output:')
+    console_output_label = Label(
+        console_output_frame,
+        anchor=NW,
+        bg='lightgrey',
+        fg='black',
+        borderwidth=5,
+        relief='sunken',
+        justify=LEFT,
+        textvariable=console_output_channel,
+        # padx=50,
+        # pady=50,
+    )
+    console_output_label.pack(side=LEFT, fill=BOTH, expand=True)
 
     # all widgets will be here
     # Execute Tkinter
     root.mainloop()
 
-    if output_folder_path.get() == 'Leave empty to use the same as input' or output_folder_path.get() == '':
-        output_folder_path.set(input_folder_path.get())
-    args_dict = {
-        'input_folder_path': input_folder_path.get(),
-        'output_folder_path': output_folder_path.get(),
-        'name_prefix': name_prefix.get(),
-        'name_suffix': name_suffix.get(),
-        'convert_only_format': [],
-        'keep_original': bool(keep_original_int.get()),
-        'verbose': True,
-    }
-    pretty_print_dict(args_dict)
 
-    return args_dict
+def process_args(args_dict: dict) -> dict:
+    input_folder_path = args_dict['input_folder_path']
+    output_folder_path = args_dict['output_folder_path']
+    console_output_channel = args_dict['console_output_channel']
+    if not os.path.exists(input_folder_path):
+        add_text_to_string_var(
+            string_var=console_output_channel,
+            text=f'ERROR: Input folder does not exists: {input_folder_path}'
+                 f'\nProgram terminated. 0 files have been renamed.')
+        return {}
 
+    if output_folder_path == '':
+        output_folder_path = input_folder_path
+    else:
+        if not os.path.exists(output_folder_path):
+            add_text_to_string_var(
+                string_var=console_output_channel,
+                text=f'ERROR: Output folder does not exists: {output_folder_path}'
+                     f'\nProgram terminated. 0 files have been renamed.')
+            return {}
 
-def rename_files(**kwargs):
-    input_folder_path = kwargs['input_folder_path']
-    output_folder_path = kwargs['output_folder_path']
     if input_folder_path[-1] != '/':
         input_folder_path += '/'
     if output_folder_path[-1] != '/':
         output_folder_path += '/'
-    name_prefix = kwargs['name_prefix']
-    name_suffix = kwargs['name_suffix']
+    name_prefix = args_dict['name_prefix']
+    name_suffix = args_dict['name_suffix']
     if name_prefix != '':
         if name_prefix[-1] != '_':
             name_prefix += '_'
     if name_suffix != '':
         if name_suffix[0] != '_':
             name_suffix = f'_{name_suffix}'
-    convert_only_format = kwargs['convert_only_format']
-    keep_original = kwargs['keep_original']
-    if kwargs['verbose']:
-        print('File renamer is running...')
-        print(f' - Select files from: {input_folder_path}')
-        print(f' - Put renamed files in: {output_folder_path}')
+    convert_only_format = args_dict['convert_only_format']
+    keep_original = args_dict['keep_original']
+    if args_dict['verbose']:
+        add_text_to_string_var(string_var=console_output_channel, text='File Renamer is running...')
+        add_text_to_string_var(string_var=console_output_channel, text='With settings:')
+        add_text_to_string_var(string_var=console_output_channel, text=f' - Select files from: {input_folder_path}')
+        add_text_to_string_var(string_var=console_output_channel, text=f' - Put renamed files in: {output_folder_path}')
         if name_prefix != '':
-            print(f' - Prefix added to all file names: {name_prefix}')
+            add_text_to_string_var(
+                string_var=console_output_channel,
+                text=f' - Prefix added to all file names: {name_prefix}',
+            )
         if name_suffix != '':
-            print(f' - Suffix added to all file names: {name_suffix}')
+            add_text_to_string_var(
+                string_var=console_output_channel,
+                text=f' - Suffix added to all file names: {name_suffix}',
+            )
         if len(convert_only_format) > 0:
-            print(f' - Convert only the following formats: {convert_only_format}')
-        print(f' - Keep original files: {keep_original}')
+            add_text_to_string_var(
+                string_var=console_output_channel,
+                text=f' - Convert only the following formats: {convert_only_format}',
+            )
+        add_text_to_string_var(string_var=console_output_channel, text=f' - Keep original files: {keep_original}')
+
+    preprocessed_args_dict = {
+        'input_folder_path': input_folder_path,
+        'output_folder_path': output_folder_path,
+        'name_prefix': name_prefix,
+        'name_suffix': name_suffix,
+        'convert_only_format': convert_only_format,
+        'keep_original': keep_original,
+        'verbose': args_dict['verbose'],
+        'console_output_channel': console_output_channel,
+    }
+    return preprocessed_args_dict
+
+
+def rename_files(**kwargs):
+    processed_kwargs = process_args(kwargs)
+    input_folder_path = processed_kwargs['input_folder_path']
+    output_folder_path = processed_kwargs['output_folder_path']
+    name_prefix = processed_kwargs['name_prefix']
+    name_suffix = processed_kwargs['name_suffix']
+    convert_only_format = processed_kwargs['convert_only_format']
+    console_output_channel = processed_kwargs['console_output_channel']
+    keep_original = processed_kwargs['keep_original']
     counter = 0
     with os.scandir(input_folder_path) as dir_entry_iterator:
         for entry in dir_entry_iterator:
@@ -156,16 +256,23 @@ def rename_files(**kwargs):
 
                 new_file_name = f'{name_prefix}{time_stamp}{name_suffix}.{file_format}'
 
+                if processed_kwargs['verbose']:
+                    add_text_to_string_var(
+                        string_var=console_output_channel,
+                        text=f'\t{entry.name} -> {new_file_name}',
+                    )
                 if keep_original:
                     shutil.copy(src=entry.path, dst=f'{output_folder_path}{new_file_name}')
                 else:
                     os.rename(src=entry.path, dst=f'{output_folder_path}{new_file_name}')
                 counter += 1
 
-    if kwargs['verbose']:
-        print(f'Execution completed: {counter} files have been renamed.')
+    if processed_kwargs['verbose']:
+        add_text_to_string_var(
+            string_var=console_output_channel,
+            text=f'Execution completed: {counter} files have been renamed.',
+        )
 
 
 if __name__ == '__main__':
-    kwargs = get_args_gui()
-    rename_files(**kwargs)
+    file_renamer_gui_()
